4c040f1a8947ab6c0f643a742a9586aafd13e5c9
Yamuna <hyamuna2807@gmail.com>
2026-04-25 21:16:04 +0530

    Add files via upload

---
+++ python11.py
@@ -0,0 +1,12 @@
+items=["dose","idli","bisibelebath","pongal","palav","poori","chitranna"]
+name=input("Enter a item")
+
+for item in items:
+    if name=="palav":
+        print("not available")
+        break
+    elif name==name:
+        print("available")
+        break
+else:
+    print("not available")        
